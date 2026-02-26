d17659e25443c8d078a60700056fddb852c3c5ed
Shahzad <111357352+Shahzadsdr55@users.noreply.github.com>
2026-02-26 18:25:31 +0000

    updated

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,28 @@
-let score = "33abc"
+let score = "Shahzad"
 
-console.log(typeof score);
-console.log(typeof(score))
+// console.log(typeof score);
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Shahzad"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+// 1 => true; 0=> false
+// "" => false
+// "Shahzad" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber)
+console.log(typeof stringNumber)
