f9a7226cd30eefc6612565c426a9a7271d74368f
Shahzad <111357352+Shahzadsdr55@users.noreply.github.com>
2026-02-27 10:20:37 +0000

    updated

---
+++ datatypes-summary.js
@@ -0,0 +1,19 @@
+// Premitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const id1 = Symbol("amsnd")
+const id2 = Symbol("amsnd")
+console.log(id1)
+console.log(id2)
+console.log(id1 === id2)
+
+const bigNumber = 281763567891987289n
+console.log(bigNumber)
+
+// Reference(Non Primitive)
+
+// Array, Objects, Functions
