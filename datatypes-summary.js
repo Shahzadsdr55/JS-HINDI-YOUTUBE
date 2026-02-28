51958f5cdf817b5505d894af82ba3116a26fd724
Shahzad <111357352+Shahzadsdr55@users.noreply.github.com>
2026-02-28 12:06:33 +0000

    updated

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -2,18 +2,34 @@
 
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const id1 = Symbol("amsnd")
-const id2 = Symbol("amsnd")
-console.log(id1)
-console.log(id2)
-console.log(id1 === id2)
+// const isLoggegIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id1 = Symbol("123")
+// const id2 = Symbol("123")
+
+// console.log(id1 === id2);
 
 const bigNumber = 281763567891987289n
-console.log(bigNumber)
+
+
 
 // Reference(Non Primitive)
 
 // Array, Objects, Functions
+
+const heros = ["H1", "H2", "H3"];
+let myObj = {
+    name: "Shahzad Khan",
+    age: 27,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber)
