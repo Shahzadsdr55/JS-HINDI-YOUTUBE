bf9cb92d5bb0a747e611c17822abdf68b2a68b5b
Shahzad <111357352+Shahzadsdr55@users.noreply.github.com>
2026-03-05 10:28:51 +0000

    updated

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,23 @@
+const name = "Shahzad"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value")
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String(" Shahzad d");
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t   '));
+// console.log(gameName.trim('t'));
+
+const url = "https://github.com/Shahzadsdr55%20/JS-HINDI-YOUTUBE"
+console.log(url.replace('%20', '-'))
+
+console.log(gameName.split('-'));
