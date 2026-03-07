a29fb789fb1695b199c608b99220831e0c5edcc1
Shahzad <111357352+Shahzadsdr55@users.noreply.github.com>
2026-03-07 10:39:15 +0000

    add 03_objects.js

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -46,10 +46,30 @@ const users = [
 ]
 
 users[1].email1
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "shahzad"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: Instructor} = course
+
+// console.log(courseInstructor);
+console.log(Instructor);
+
+{
+    "name": "shahzad",
+    "coursename": "js in hindi",
+    "price": "free"
+}
