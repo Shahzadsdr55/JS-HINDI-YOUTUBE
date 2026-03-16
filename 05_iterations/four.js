a1d00994418f9f19f94dfacd667c1f316386d5b6
Shahzad <111357352+Shahzadsdr55@users.noreply.github.com>
2026-03-16 17:00:49 +0000

    adding files

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -6,5 +6,22 @@ const myObject = {
 }
 
 for (const key in myObject) {
-
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
 }
+
+const progeramming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in progeramming) {
+    // console.log(progeramming[key]);
+}
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "UNITED STATE OF AMARICA")
+map.set('FR', "FRANCE")
+map.set('IN', "India")
+
+for (const key in map) {
+    console.log(key);
+}
+
