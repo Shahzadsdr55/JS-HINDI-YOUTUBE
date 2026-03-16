c97d6400518f31eafca6bb8ac3e471bc3f1ce8be
Shahzad <111357352+Shahzadsdr55@users.noreply.github.com>
2026-03-16 17:06:13 +0000

    ipdated

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -26,14 +26,16 @@ const myCoding = [
 
     },
     {
-      
+        languageName: "java",
+        languageFileName: "java" 
     },
     {
-      
+        languageName: "python",
+        languageFileName: "py"
     },
 ]
 
 myCoding.forEach( (item) => {
 
-    console.log(item.languageFileName);
+    console.log(item.languageName);
 } )
